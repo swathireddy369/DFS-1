c6b4534666eaa16a55458e90779c0f859f1855db
swathireddy369 <swathiamaravadi53@gmail.com>
2025-02-21 21:38:36 -0500

    DFS-1

--- 01-matrix.java
+++ 01-matrix.java
@@ -46,3 +46,47 @@ class Solution {
         return mat;           
     }
 }
+
+
+// Time Complexity :O(m*n)
+// Space Complexity :O(m*n)
+// Did this code successfully run on Leetcode :yes
+// Any problem you faced while coding this :no
+
+
+// Your code here along with comments explaining your approach:here same as above approach but the difference is i am not using any distance or size ,because for every next element the distance would current+1 because we are considering 0 first.
+class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+        int m=mat.length;
+        int n=mat[0].length;
+        Queue<Integer> q1=new LinkedList<>();
+         Queue<Integer> q2=new LinkedList<>();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j] == 0){
+                    q1.add(i);
+                    q2.add(j);
+                 
+                }else{
+                    mat[i][j]=-1;
+                }
+        }
+        }
+        int[][] dirs={{0,1},{0,-1},{1,0},{-1,0}};
+        while(!q1.isEmpty()){
+             int cr=q1.poll(); 
+             int cc=q2.poll();
+             for(int[] dir:dirs){
+                int nr=cr+dir[0];
+                int nc=cc+dir[1];
+                 if(nr >= 0 && nr < m && nc >=0 && nc<n && mat[nr][nc] == -1 ){
+                   q1.add(nr);
+                  q2.add(nc);
+                  mat[nr][nc] = mat[cr][cc]+1;
+             }   
+            }
+                       
+        }
+        return mat;           
+    }
+}
